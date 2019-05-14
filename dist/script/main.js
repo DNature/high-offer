7b430693884a439e9de0b91b6a3119dd6326f76d
Divine Nature <divinehycenth8@gmail.com>
2019-05-14 22:31:03 +0100

    Completed desktop version

---
+++ dist/script/main.js
@@ -0,0 +1,17 @@
+// === client section slider
+var swiper = new Swiper('.swiper-container', {
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: {
+		delay: 6500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
